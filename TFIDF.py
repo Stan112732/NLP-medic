dd5a50a27bebb57d18a1ad7787783a6fbb2ccd13
Stan112732 <james112732@gmail.com>
2022-08-02 22:00:26 +0200

    8.2 第二次修改 解决分词问题 筛选了约150个词作为种类

--- TFIDF.py
+++ TFIDF.py
@@ -7,17 +7,18 @@ import pandas as pd
 
 corpus = open('ct.txt', encoding='utf-8')#获取所有医疗器材
 stopwords = open('stopwords.txt', encoding='utf-8')#分词
-corpus = [list(jieba.cut(c)) for c in corpus if c not in stopwords]#清除stopwords
+stopword_list = [word.strip('\n') for word in stopwords.readlines()]
+corpus = [list(jieba.cut(c)) for c in corpus]
 print(corpus)
 #引用包进行TFIDF值计算
-vectorizer = TfidfVectorizer(lowercase=False, tokenizer = lambda doc: doc)
+vectorizer = TfidfVectorizer(lowercase=False, tokenizer = lambda doc: doc,stop_words=stopword_list,max_features=150)#设置stopword和获取词的阈值
 tdm = vectorizer.fit_transform(corpus)
 names = {'name':vectorizer.get_feature_names()}
-print(tdm.toarray().sum(axis=0))
+#print(tdm.toarray().sum(axis=0))
 #创建DF并将物品名与其TFIDF值赋予DF
 data={'word':vectorizer.get_feature_names(),
       'tfidf':tdm.toarray().sum(axis=0).tolist()}
 df=pd.DataFrame(data)
-df.sort_values(by="tfidf",ascending=True)#排序（无效）
+df.sort_values(by="tfidf",ascending=True)
 print(df)
-df.to_csv('F:/STAGE/tfidf.csv',encoding='utf_8_sig')
+df.to_csv('F:/STAGE/tfidf1.csv',encoding='utf_8_sig')
