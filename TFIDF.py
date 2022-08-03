7bdaa33c84c6ad0a5016593939e1dfe6c853a48d
Stan112732 <james112732@gmail.com>
2022-08-03 22:46:42 +0200

    8.3 第三次修改 增加 tfidf抽取矩阵 增加 kmeans聚类 目前可以看到每个样本对应的簇编号 接下来解决结果处理 使结果能让碳基生物看懂

--- TFIDF.py
+++ TFIDF.py
@@ -2,23 +2,44 @@
 #目前可以计算所有TFIDF值，但尚未确定具体哪些作为种类
 
 import jieba
+from sklearn import cluster
 from sklearn.feature_extraction.text import TfidfVectorizer,CountVectorizer
 import pandas as pd
+import numpy as np
 
 corpus = open('ct.txt', encoding='utf-8')#获取所有医疗器材
 stopwords = open('stopwords.txt', encoding='utf-8')#分词
 stopword_list = [word.strip('\n') for word in stopwords.readlines()]
 corpus = [list(jieba.cut(c)) for c in corpus]
-print(corpus)
+#print(corpus)
 #引用包进行TFIDF值计算
-vectorizer = TfidfVectorizer(lowercase=False, tokenizer = lambda doc: doc,stop_words=stopword_list,max_features=150)#设置stopword和获取词的阈值
+vectorizer = TfidfVectorizer(lowercase=False, tokenizer = lambda doc: doc,stop_words=stopword_list)#设置stopword和获取词的阈值
 tdm = vectorizer.fit_transform(corpus)
-names = {'name':vectorizer.get_feature_names()}
 #print(tdm.toarray().sum(axis=0))
+word=vectorizer.get_feature_names()#获取词袋模型中的所有词语
+features=tdm.toarray()#将tf-idf矩阵抽取出来，元素a[i][j]表示j词在i类文本中的tf-idf权重
+
+km = cluster.KMeans(n_clusters=200, random_state=100,max_iter=500)
+#放入上矩阵进行 kmeans 聚类
+c=km.fit(features)
+print(c)
+t = c.labels_ # t 存储每个样本所属的簇
+i = 1
+while i <= len(km.labels_):
+      print(i, km.labels_[i - 1])#（样本序号 簇）
+      i = i + 1
+print(t)#簇
+print(km.inertia_)
+res = pd.DataFrame(c)
+res.to_csv('./res.csv')
+
+
+
+
 #创建DF并将物品名与其TFIDF值赋予DF
-data={'word':vectorizer.get_feature_names(),
-      'tfidf':tdm.toarray().sum(axis=0).tolist()}
-df=pd.DataFrame(data)
-df.sort_values(by="tfidf",ascending=True)
-print(df)
-df.to_csv('F:/STAGE/tfidf1.csv',encoding='utf_8_sig')
+# data={'word':vectorizer.get_feature_names(),
+#       'tfidf':tdm.toarray().sum(axis=0).tolist()}
+# df=pd.DataFrame(data)
+# df.sort_values(by="tfidf",ascending=True)
+# print(df)
+# df.to_csv('F:/STAGE/objects.csv',encoding='utf_8_sig')
